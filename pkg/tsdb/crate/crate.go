fe3cbe1216c00ff0f610e681744c8152a452f3f1
Gabriele Cerfoglio <cerfoglg@Gabrieles-MacBook-Pro.local>
2018-06-06 16:12:12 +0200

    Handling multiple queries, cleaned up from fmt prints

--- pkg/tsdb/crate/crate.go
+++ pkg/tsdb/crate/crate.go
@@ -2,13 +2,10 @@ package crate
 
 import (
 	"context"
-	"fmt"
+	_ "fmt"
 	_ "path"
 	"strconv"
 	_ "strings"
-
-	_ "golang.org/x/net/context/ctxhttp"
-
 	_ "encoding/json"
 	_ "io/ioutil"
 	_ "net/http"
@@ -40,21 +37,18 @@ func init() {
 }
 
 func (e *CrateExecutor) Query(ctx context.Context, dsInfo *models.DataSource, queryContext *tsdb.TsdbQuery) (*tsdb.Response, error) {
-	fmt.Println("Context: ", ctx)
-	fmt.Println("DSInfo: ", dsInfo)
 	dsJson, err := dsInfo.JsonData.Map()
 	if err != nil {
 		plog.Info("error", err)
 	}
-	fmt.Println("DSJson: ", dsJson)
+
 	result := &tsdb.Response{}
 
 	start := queryContext.TimeRange.GetFromAsMsEpoch()
 	startTime := strconv.FormatInt(start, 10)
-	fmt.Println("Start: ", start)
+
 	end := queryContext.TimeRange.GetToAsMsEpoch()
 	endTime := strconv.FormatInt(end, 10)
-	fmt.Println("End: ", end)
 
 	db, err := sql.Open("crate", dsInfo.Url)
 	if err != nil {
@@ -63,54 +57,64 @@ func (e *CrateExecutor) Query(ctx context.Context, dsInfo *models.DataSource, qu
 
 	queryResults := make(map[string]*tsdb.QueryResult)
 
+	timeColumn := dsJson["timeColumn"].(string)
+	schema := dsJson["schema"].(string)
+	table := dsJson["table"].(string)
+
 	for _, query := range queryContext.Queries {
 		q, err := query.Model.Map()
 		if err != nil {
 			plog.Info("error", err)
 		}
+
+		refID := q["refId"].(string)
 		m, err := query.Model.Get("metricAggs").Array()
 		if err != nil {
 			plog.Info("error", err)
 		}
-		metricColumn := m[0].(map[string]interface{})["column"].(string)
-		timeColumn := dsJson["timeColumn"].(string)
-		schema := dsJson["schema"].(string)
-		table := dsJson["table"].(string)
-		refID := q["refId"].(string)
-		fmt.Println("Full Query: ", q)
-		fmt.Println("Query: ", m[0].(map[string]interface{})["column"])
-		rows, err := db.Query("SELECT "+timeColumn+","+metricColumn+" FROM "+schema+"."+table+" WHERE "+timeColumn+">"+startTime+" AND "+timeColumn+"<"+endTime+";")
-		if err != nil {
-			plog.Info("error", err)
-		}
-		defer rows.Close()
+
 		queryRes := tsdb.NewQueryResult()
-		series := tsdb.TimeSeries{
-			Name: metricColumn,
-		}
-		for rows.Next() {
-			var timeValue string
-			var metricValue string
-			if err := rows.Scan(&timeValue, &metricValue); err != nil {
-				plog.Info("error", err)
-			}
-			floatTimeValue, err := strconv.ParseFloat(timeValue, 64)
+
+		for i, _ := range m {
+			metricColumn := m[i].(map[string]interface{})["column"].(string)
+
+			rows, err := db.Query("SELECT "+timeColumn+","+metricColumn+" FROM "+schema+"."+table+" WHERE "+timeColumn+">"+startTime+" AND "+timeColumn+"<"+endTime+";")
 			if err != nil {
 				plog.Info("error", err)
 			}
-			floatMetricValue, err := strconv.ParseFloat(metricValue, 64)
-			if err != nil {
+			defer rows.Close()
+
+			series := tsdb.TimeSeries{
+				Name: metricColumn,
+			}
+
+			for rows.Next() {
+				var timeValue string
+				var metricValue string
+
+				if err := rows.Scan(&timeValue, &metricValue); err != nil {
+					plog.Info("error", err)
+				}
+
+				floatTimeValue, err := strconv.ParseFloat(timeValue, 64)
+				if err != nil {
+					plog.Info("error", err)
+				}
+
+				floatMetricValue, err := strconv.ParseFloat(metricValue, 64)
+				if err != nil {
+					plog.Info("error", err)
+				}
+
+				series.Points = append(series.Points, tsdb.NewTimePoint(null.FloatFrom(floatMetricValue), floatTimeValue))
+			}
+			if err := rows.Err(); err != nil {
 				plog.Info("error", err)
 			}
-			series.Points = append(series.Points, tsdb.NewTimePoint(null.FloatFrom(floatMetricValue), floatTimeValue))
+			queryRes.Series = append(queryRes.Series, &series)
 		}
-		if err := rows.Err(); err != nil {
-			plog.Info("error", err)
-		}
-		queryRes.Series = append(queryRes.Series, &series)
 		queryResults[refID] = queryRes
 	}
-
 	result.Results = queryResults
 	return result, nil
 }
