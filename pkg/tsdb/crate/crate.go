57ac9cc24a1379b233ec7a4c2a52492666fc6030
Gabriele Cerfoglio <gabrielecerfoglio225@gmail.com>
2018-06-07 16:32:35 +0200

    Oc cratedb alerting enhancements (#5)
    * Handling multiple queries, cleaned up from fmt prints
    
    * Where clauses with better logging, simple grouping in queries

--- pkg/tsdb/crate/crate.go
+++ pkg/tsdb/crate/crate.go
@@ -2,13 +2,9 @@ package crate
 
 import (
 	"context"
-	"fmt"
 	_ "path"
 	"strconv"
 	_ "strings"
-
-	_ "golang.org/x/net/context/ctxhttp"
-
 	_ "encoding/json"
 	_ "io/ioutil"
 	_ "net/http"
@@ -40,21 +36,21 @@ func init() {
 }
 
 func (e *CrateExecutor) Query(ctx context.Context, dsInfo *models.DataSource, queryContext *tsdb.TsdbQuery) (*tsdb.Response, error) {
-	fmt.Println("Context: ", ctx)
-	fmt.Println("DSInfo: ", dsInfo)
 	dsJson, err := dsInfo.JsonData.Map()
 	if err != nil {
 		plog.Info("error", err)
 	}
-	fmt.Println("DSJson: ", dsJson)
+	plog.Info("Alert Context: ", ctx)
+	plog.Info("Alert DSInfo: ", dsInfo)
+	plog.Info("Alert DSJson: ", dsJson)
+
 	result := &tsdb.Response{}
 
 	start := queryContext.TimeRange.GetFromAsMsEpoch()
 	startTime := strconv.FormatInt(start, 10)
-	fmt.Println("Start: ", start)
+
 	end := queryContext.TimeRange.GetToAsMsEpoch()
 	endTime := strconv.FormatInt(end, 10)
-	fmt.Println("End: ", end)
 
 	db, err := sql.Open("crate", dsInfo.Url)
 	if err != nil {
@@ -63,54 +59,97 @@ func (e *CrateExecutor) Query(ctx context.Context, dsInfo *models.DataSource, qu
 
 	queryResults := make(map[string]*tsdb.QueryResult)
 
+	timeColumn := dsJson["timeColumn"].(string)
+	schema := dsJson["schema"].(string)
+	table := dsJson["table"].(string)
+
 	for _, query := range queryContext.Queries {
 		q, err := query.Model.Map()
 		if err != nil {
 			plog.Info("error", err)
 		}
+		plog.Info("Alert Query: ", q)
+
+		refID := q["refId"].(string)
+
 		m, err := query.Model.Get("metricAggs").Array()
 		if err != nil {
 			plog.Info("error", err)
 		}
-		metricColumn := m[0].(map[string]interface{})["column"].(string)
-		timeColumn := dsJson["timeColumn"].(string)
-		schema := dsJson["schema"].(string)
-		table := dsJson["table"].(string)
-		refID := q["refId"].(string)
-		fmt.Println("Full Query: ", q)
-		fmt.Println("Query: ", m[0].(map[string]interface{})["column"])
-		rows, err := db.Query("SELECT "+timeColumn+","+metricColumn+" FROM "+schema+"."+table+" WHERE "+timeColumn+">"+startTime+" AND "+timeColumn+"<"+endTime+";")
+
+		whereClauses, err := query.Model.Get("whereClauses").Array()
 		if err != nil {
 			plog.Info("error", err)
 		}
-		defer rows.Close()
-		queryRes := tsdb.NewQueryResult()
-		series := tsdb.TimeSeries{
-			Name: metricColumn,
-		}
-		for rows.Next() {
-			var timeValue string
-			var metricValue string
-			if err := rows.Scan(&timeValue, &metricValue); err != nil {
-				plog.Info("error", err)
-			}
-			floatTimeValue, err := strconv.ParseFloat(timeValue, 64)
-			if err != nil {
-				plog.Info("error", err)
-			}
-			floatMetricValue, err := strconv.ParseFloat(metricValue, 64)
-			if err != nil {
-				plog.Info("error", err)
-			}
-			series.Points = append(series.Points, tsdb.NewTimePoint(null.FloatFrom(floatMetricValue), floatTimeValue))
-		}
-		if err := rows.Err(); err != nil {
+
+		groupByColumns, err := query.Model.Get("groupByColumns").Array()
+		if err != nil {
 			plog.Info("error", err)
 		}
-		queryRes.Series = append(queryRes.Series, &series)
+
+		queryRes := tsdb.NewQueryResult()
+
+		for i, _ := range m {
+			metricColumn := m[i].(map[string]interface{})["column"].(string)
+
+			queryString := "SELECT "+timeColumn+","+metricColumn+" FROM "+schema+"."+table+" WHERE "+timeColumn+">"+startTime+" AND "+timeColumn+"<"+endTime
+			for i, _ := range whereClauses {
+				c := whereClauses[i].(map[string]interface{})
+				cond := "AND"
+				if c["condition"].(string) != "" {
+					cond = c["condition"].(string)
+				}
+				queryString = queryString+" "+cond+" "+c["column"].(string)+c["operator"].(string)+c["value"].(string)
+			}
+			if len(groupByColumns) != 0 {
+				queryString = queryString+" GROUP BY "+timeColumn+","+metricColumn
+				for i, _ := range groupByColumns {
+					if groupByColumns[i].(string) != metricColumn && groupByColumns[i].(string) != timeColumn {
+						queryString = queryString+","+groupByColumns[i].(string)
+					}
+				}
+			}
+			queryString = queryString+";"
+			plog.Info("Alert Query String: ", queryString)
+
+			rows, err := db.Query(queryString)
+			if err != nil {
+				plog.Info("error", err)
+				continue
+			}
+			defer rows.Close()
+
+			series := tsdb.TimeSeries{
+				Name: metricColumn,
+			}
+
+			for rows.Next() {
+				var timeValue string
+				var metricValue string
+
+				if err := rows.Scan(&timeValue, &metricValue); err != nil {
+					plog.Info("error", err)
+				}
+
+				floatTimeValue, err := strconv.ParseFloat(timeValue, 64)
+				if err != nil {
+					plog.Info("error", err)
+				}
+
+				floatMetricValue, err := strconv.ParseFloat(metricValue, 64)
+				if err != nil {
+					plog.Info("error", err)
+				}
+
+				series.Points = append(series.Points, tsdb.NewTimePoint(null.FloatFrom(floatMetricValue), floatTimeValue))
+			}
+			if err := rows.Err(); err != nil {
+				plog.Info("error", err)
+			}
+			queryRes.Series = append(queryRes.Series, &series)
+		}
 		queryResults[refID] = queryRes
 	}
-
 	result.Results = queryResults
 	return result, nil
 }
